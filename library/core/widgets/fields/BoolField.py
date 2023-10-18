2fb2f39281385f8ac3478a37aaf3e775be02fc58
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-10-18 16:22:35 +0300

    fix only one annotation

--- library/core/widgets/fields/BoolField.py
+++ library/core/widgets/fields/BoolField.py
@@ -1,10 +1,11 @@
 import flet as ft
+from typing import Union
 
 
 class BoolField(ft.Checkbox):
     def __init__(
         self,
-        value: bool | None = False,
+        value: Union[bool, None] = False,
         tristate: bool = False,
         label_position: ft.LabelPosition = ft.LabelPosition.RIGHT,
         label: str = '',
